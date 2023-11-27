86f0bd9496aabb220cf56d9967b9dbe0693a2094
Ninakhom <ninakhom206@gmail.com>
2023-11-28 00:06:19 +0700

    init

--- ConnectDB.py
+++ ConnectDB.py
@@ -6,7 +6,7 @@ def create_connection():
             host="localhost",
             user="root",
             password="",
-            database="res"
+            database=""
         )
         print("Connected to the database")
         return connection
