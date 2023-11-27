700e55752c50a37ad9feec10e80058bc9099bac1
Ninakhom <ninakhom206@gmail.com>
2023-11-27 18:41:37 +0700

    init

---
+++ ConnectDB.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+def create_connection():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="Python_GUI"
+        )
+        print("Connected to the database")
+        return connection
+    except mysql.connector.Error as e:
+        print(e)
+        return None
+
+def close_connection(connection):
+
+    if connection:
+        connection.close()
+        print("Connection closed")
+
+def get_cursor(connection):
+
+    if connection:
+        return connection.cursor()
+    else:
+        return None
---
+++ Login.py
@@ -0,0 +1,4 @@
+from ConnectDB import create_connection, close_connection, get_cursor
+conn = create_connection()
+cursor = get_cursor(conn)
+
